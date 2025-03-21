11ab4102a4b619ce605db852846fc7ca19e811db
Rashmi Ranjan Parija <rashmiranjanparija78898@gmail.com>
2025-03-21 19:13:33 +0530

    Implement search functionality in SearchPage component to fetch and display courses based on user input

--- src/app/(user)/search/page.tsx
+++ src/app/(user)/search/page.tsx
@@ -1,11 +1,19 @@
 // import { getSearchResults } from '@/lib/search'
 
+import { searchCourses } from "@/sanity/lib/courses/searchCourses"
+import { redirect } from "next/navigation"
+
 export default async function SearchPage({
     searchParams,
 }: {
     searchParams: Promise<{ [key: string]: string | string[] | undefined }>
 }) {
-    const term =(await searchParams).term
-
-    return <div>{term}</div>
+    const term = (await searchParams).term
+    if (!term || typeof term !== "string") {
+        return redirect("/")
+    }
+    const decodedTerm = decodeURIComponent(term)
+    const courses = await searchCourses(decodedTerm)
+    console.log(courses)
+    return <div>{courses.map(course => <div key={course._id}>{course.title}</div>)}</div>
 }

4a3fdf566a78630888f133571647ecc291e016dc
Rashmi Ranjan Parija <rashmiranjanparija78898@gmail.com>
2025-03-23 19:27:22 +0530

    Fetch course details by slug and handle authentication in CoursePage component

--- src/app/(user)/courses/[slug]/page.tsx
+++ src/app/(user)/courses/[slug]/page.tsx
@@ -1,13 +1,20 @@
+import getCourseBySlug from '@/sanity/lib/courses/getCoursesBySlug';
+import { auth } from '@clerk/nextjs/server';
 import React from 'react'
 
 interface CoursePageProps {
-    params: {
+    params: Promise<{
         slug: string;
-    };
+    }>;
 }
 
-const CoursePage = ({ params }: CoursePageProps) => {
-    const { slug } = params;
+const CoursePage = async({ params }: CoursePageProps) => {
+    const { slug } = await params;
+    const course = await getCourseBySlug(slug);
+    const { userId } = await auth();
+if (!course){
+    return <div>Course not found</div>
+}
     return <div>CoursePage: {slug}</div>;
 };
 

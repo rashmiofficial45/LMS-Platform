766b2a352d34b9a479764e1e5e961ec9f70aca26
Rashmi Ranjan Parija <rashmiranjanparija78898@gmail.com>
2025-03-27 23:29:35 +0530

    validating the course data and preparing the price for stripe

--- action/createStripeCheckout.ts
+++ action/createStripeCheckout.ts
@@ -2,6 +2,7 @@
 import baseUrl from "@/lib/baseUrl";
 import stripe from "@/lib/stripe";
 import getCourseById from "@/sanity/lib/courses/getCourseById";
+import { createEnrollment } from "@/sanity/lib/student/createEnrollment";
 import { createStudentIfNotExists } from "@/sanity/lib/student/createStudentIfNotExists";
 import { clerkClient } from "@clerk/nextjs/server";
 
@@ -31,5 +32,26 @@ export default async function createStripeCheckout(
     throw new Error("User not found");
   }
 
-  
+  // 2. Validate course data and prepare price for Stripe
+  if (!course.price || course.price !== 0) {
+    throw new Error("Course price is not set");
+  }
+  const priceInCents = Math.round(course.price * 100);
+  // if course is free, create enrollment and redirect to course page (BYPASS STRIPE CHECKOUT)
+  if (priceInCents === 0) {
+    await createEnrollment({
+      studentId: userId,
+      courseId: courseId,
+      amount: 0,
+      paymentId: "free",
+    });
+    return {
+      url: `/courses/${course.slug?.current}`,
+    };
+  }
+  const { title, description, image, slug } = course;
+
+  if (!title || !description || !image || !slug) {
+    throw new Error("Course data is incomplete");
+  }
 }
